57c58e4bd70028ef50b517f7d1db561213961bba
mattb12321 <matti.manof@gmail.com>
2022-06-25 00:08:59 +0300

    Added all features required for search fragment: xml's, recyclerView, SearchAdapter. Also added assets required to populate the recyclerView

---
+++ app/src/main/java/com/example/musicapp/SearchAdapter.java
@@ -0,0 +1,106 @@
+package com.example.musicapp;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>{
+    ArrayList<Song> allSongs;
+    ArrayList<Song> shownSongs;
+    Context context;
+    SearchAdapterListener listener;
+    MediaPlayer mp;
+
+    public SearchAdapter(Context context, SearchAdapterListener listener, ArrayList<Song> shownSongs) {
+        this.context = context;
+        this.listener = listener;
+        this.allSongs = SongsCSVParser.parseSongs(context);
+        this.shownSongs = shownSongs;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.row_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.shownSongs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        View itemView;
+        TextView songName;
+        TextView artistName;
+        TextView albumName;
+        ImageView albumArt;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            itemView.setClickable(true);
+            songName = (TextView) itemView.findViewById(R.id.song_name);
+            artistName = (TextView) itemView.findViewById(R.id.artist_name);
+            albumName = (TextView) itemView.findViewById(R.id.album_name);
+            albumArt = (ImageView) itemView.findViewById(R.id.album_art);
+        }
+
+        public void bindData(final int position) {
+            songName.setText(shownSongs.get(position).name);
+            artistName.setText(shownSongs.get(position).artist);
+            albumName.setText(shownSongs.get(position).album);
+            albumArt.setImageResource(context.getResources().getIdentifier(
+                    shownSongs.get(position).art,"drawable", context.getPackageName()
+            ));
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    onItemLongClick(position);
+                    return false;
+                }
+            });
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClick(position);
+                }
+            });
+        }
+    }
+    public void onItemLongClick(int position) {
+        Song song = this.shownSongs.get(position);
+        //TODO: add code for saving to favorites
+        notifyDataSetChanged();
+    }
+
+    public void onItemClick(int position) {
+        Song song = this.shownSongs.get(position);
+        if(mp != null)
+            mp.stop();
+        mp = MediaPlayer.create(this.context,
+                this.context.getResources().getIdentifier(song.track, "raw", this.context.getPackageName()));
+        mp.start();
+    }
+
+    public interface SearchAdapterListener {
+        public void updateShownSongsList(boolean toAdd, String name);
+    }
+}
--- app/src/main/java/com/example/musicapp/SearchFragment.java
+++ app/src/main/java/com/example/musicapp/SearchFragment.java
@@ -1,18 +1,100 @@
 package com.example.musicapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class SearchFragment extends Fragment {
+    RecyclerView rvSongs;
+    ArrayList<Song> shownSongsList;
+    SearchAdapter adapter;
+    SearchAdapter.SearchAdapterListener listener;
+    ArrayList<Song> allSongsList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search,container,false);
+        allSongsList = SongsCSVParser.parseSongs(inflater.getContext());
+        shownSongsList = new ArrayList<>();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        rvSongs = view.findViewById(R.id.searchRv);
+        SearchView searchView = (SearchView) getView().findViewById(R.id.searchView);
+        searchView.setQueryHint("Enter song name");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                shownSongsList.clear();
+                if(newText != null && !newText.trim().isEmpty()) {
+                    for (Song song:allSongsList) {
+                        if(StringUtils.containsIgnoreCase(song.name,newText))
+                            shownSongsList.add(song);
+                    }
+                }
+                adapter.shownSongs = shownSongsList;
+                rvSongs.setAdapter(adapter);
+                rvSongs.setLayoutManager(new LinearLayoutManager(getActivity()));
+                return false;
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+/*        if(context instanceof SearchAdapter.SearchAdapterListener) {
+            listener = (SearchAdapter.SearchAdapterListener)context;
+        } else {
+            throw new RuntimeException(context.toString() +
+                    "must implement SearchAdapterListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //listener = null;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //if(shownSongsNames == null) {
+            //shownSongsList = new ArrayList<Song>();
+            adapter = new SearchAdapter(getActivity(),this.listener, this.shownSongsList);
+            rvSongs.setAdapter(adapter);
+            rvSongs.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //}
     }
 }
